a27e6563bcfb03c30da325295dbf91b1d6298d68
gabrielapaz01 <gabiipaz200139@gmail.com>
2023-11-16 12:51:27 -0300

    Pusheando ML

---
+++ src/app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+//console.log('Estamos por aquí');
+//console.log(app);
+
+//Creando nuestras rutas
+app.get('/', function(req,res){
+   res.sendFile(path.join(__dirname,'/views/home.html'));
+})
+app.get('/registro', function(req,res){
+    res.sendFile(path.join(__dirname,'views/registro.html'));
+})
+app.get('/login', function(req,res){
+    res.sendFile(path.join(__dirname,'views/login.html'));
+})
+
+
+app.use(express.static(path.resolve(__dirname, "../public")));
+
+//Levantar el servidor
+app.listen(3030, function(){
+    console.log('Servidor corriendo en el puerto 3030');
+})
